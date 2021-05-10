bd2d4d502de3e4e2b3c9f632731144ff819b31ad
Quinton <qnistico@aol.com>
2021-05-09 20:19:31 -0400

    good

--- script.js
+++ script.js
@@ -6,20 +6,19 @@ let planet_top_right = document.getElementById('planet_top_right');
 let planet_middle_left = document.getElementById('planet_middle_left');
 let planet_middle_right = document.getElementById('planet_middle_right');
 let planet_bottom_right = document.getElementById('planet_bottom_right');
-let text = document.getElementById('text');
 let btn = document.getElementById('btn');
+let hero_content = document.getElementById('hero_content');
 
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
     stars.style.left = value * 0.5 + 'px';
-    moon.style.top = value * .75 + 'px';
-    small_planet.style.top = value * 2 + 'px';
-    planet_top_left.style.top = value * 0.5 + 'px';
-    planet_middle_left.style.left = value * 1.5 + 'px';
-    planet_middle_right.style.top = value * -0.618 + 'px';
+   // moon.style.top = value * .75 + 'px';
+    small_planet.style.top = value * 0.35 + 'px';
+    planet_top_left.style.top = value * -0.1 + 'px';
+    planet_middle_left.style.top = value * 0.618 + 'px';
+    planet_middle_left.style.left = value * -0.618 + 'px';
+    planet_middle_right.style.top = value * 0.618 + 'px';
     planet_top_right.style.top = value * 1.25 + 'px';
-    planet_bottom_right.style.left = value * -1.5 + 'px';
-    text.style.marginTop = value * 1.65 + 'px';
-    btn.style.marginTop = value * 1.75 + 'px';
-    mountains_front.style.marginTop = value * 0.38 + 'px';
+    planet_bottom_right.style.left = value * -1.25 + 'px';
+    hero_content.style.marginBottom = value * -1+ 'px';
 })
