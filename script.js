acbed8bc48a0f2ea5d4119fcf780148d4ff1404c
Quinton <qnistico@aol.com>
2021-05-10 23:49:42 -0400

    good

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ let planet_middle_left = document.getElementById('planet_middle_left');
 let planet_middle_right = document.getElementById('planet_middle_right');
 let btn = document.getElementById('btn');
 let hero_content = document.getElementById('hero_content');
+let c1_flex_child = document.getElementById('c1_flex_child');
+let c1_img = document.getElementById('c1_img');
+
 
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
@@ -19,11 +22,42 @@ window.addEventListener('scroll', function(){
     small_planet.style.top = value * 0.35 + 'px';
     planet_top_left.style.top = value * -0.1 + 'px';
     planet_middle_left.style.top = value * 0.618 + 'px';
-    planet_middle_left.style.animationPlayState = 'paused';
-
     planet_middle_left.style.left = value * -0.318 + 'px';
     planet_middle_right.style.top = value * .618 + 'px';
     planet_top_right.style.top = value * 0.618 + 'px';
     hero_content.style.marginBottom = value * -1+ 'px';
+    c1_flex_child.style.marginTop = value * 0.25 + 'px';
+    c1_img.style.marginTop = value * 0.1 + 'px';
+    
 })
 
+var timeoutId
+window.addEventListener('scroll', function() {
+  let value = window.scrollY;
+  clearTimeout(timeoutId);
+  
+  small_planet.style.animationPlayState = 'paused';
+  planet_top_left.style.animationPlayState = 'paused';
+ planet_middle_left.style.animationPlayState = 'paused';
+ planet_middle_right.style.animationPlayState = 'paused';
+ planet_top_right.style.animationPlayState = 'paused';
+
+  
+  timeoutId = setTimeout(function() {
+    small_planet.style.animationPlayState = 'running';
+    planet_top_left.style.animationPlayState = 'running';
+   planet_middle_left.style.animationPlayState = 'running';
+   planet_middle_right.style.animationPlayState = 'running';
+   planet_top_right.style.animationPlayState = 'running';
+  }, 100)
+
+})
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() >= 1) {
+      $('.navbar').css('background', '#1c0b38');
+  } else {
+      $('.navbar').css('background', 'transparent');
+  }
+  
+});
